170b9a3f43dbf43175c122ed5347867be2c7077a
raven <vorontsof@mail.ru>
2022-03-08 00:12:54 +0400

    lesson4  :bug:Hotfix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.sort_by_category(category)
-    Test.joins("JOIN categories c ON c.id = tests.category_id")
-        .where("c.title = ?", category).order(title: :desc).pluck(:title)
+    Test.joins("JOIN categories ON categories.id = tests.category_id")
+        .where("categories.title = ?", category).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.joins('LEFT JOIN passed_tests pt ON pt.test.id = test.id')
+    Test.joins('LEFT JOIN passed_tests ON passed_tests.test_id = test.id')
         .where(passed_tests: { user_id: self.id }, level: level)
   end
 end
