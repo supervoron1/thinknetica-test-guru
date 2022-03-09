b41e62b5bbd72f61b7aafb3acdf2986e7eae8aa8
raven <vorontsof@mail.ru>
2022-03-09 15:01:22 +0400

    lesson5  Model associations added

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
+
   def self.sort_by_category(category)
     Test.joins("JOIN categories ON categories.id = tests.category_id")
         .where("categories.title = ?", category).order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :passed_tests
+  has_many :tests, through: :passed_tests
+
   def tests_by_level(level)
     Test.joins('LEFT JOIN passed_tests ON passed_tests.test_id = test.id')
         .where(passed_tests: { user_id: self.id }, level: level)
