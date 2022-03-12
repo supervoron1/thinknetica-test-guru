5a0687d4da20c40a016d3f2c0d1f6cfb97a3eeda
raven <vorontsof@mail.ru>
2022-03-12 14:36:51 +0400

    lesson6  Scope methods added

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,10 @@ class Test < ApplicationRecord
   has_many :passed_tests
   has_many :users, through: :passed_tests
 
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :advanced, -> { where(level: 5..Float::INFINITY) }
+
   def self.sort_by_category(category)
     Test.joins("JOIN categories ON categories.id = tests.category_id")
         .where("categories.title = ?", category).order(title: :desc).pluck(:title)
