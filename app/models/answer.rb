01d68217fb2f255ab68d6c76cc99554912564e6e
Dmitriy Vorontsov <vorontsof@mail.ru>
2022-03-17 09:05:56 +0400

    lesson6  Style guide fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,12 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :correct, -> { where(correct: true) }
-
   validates :body, presence: true
   validate :validate_max_answers
+
+  scope :correct, -> { where(correct: true) }
+
+  private
 
   def validate_max_answers
     errors.add(:question, "Each question can't have more than 4 answers") if question.answers.size > 4
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,14 +5,14 @@ class Test < ApplicationRecord
   has_many :passed_tests
   has_many :users, through: :passed_tests
 
+  # also needs migration to add unique indexes to attributes: title and level
+  validates :title, presence: true, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :advanced, -> { where(level: 5..Float::INFINITY) }
   scope :by_category, -> (category_title) { joins(:category).where(categories: { title: category_title }) }
-
-  # also needs migration to add unique indexes to attributes: title and level
-  validates :title, presence: true, uniqueness: { scope: :level }
-  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def self.sort_by_category(category_title)
     # Test.joins("JOIN categories ON categories.id = tests.category_id")
