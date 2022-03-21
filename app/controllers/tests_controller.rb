74b962d5a1c95871a3f9d945cd07bcdaf8c79223
raven <vorontsof@mail.ru>
2022-03-22 00:10:32 +0400

    lesson8  Update action and view added

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -9,18 +9,33 @@ class TestsController < ApplicationController
     @test = Test.find(params[:id])
   end
 
-  def edit; end
-
+  # New -> Create
   def new
     @test = Test.new
   end
 
   def create
     @test = Test.new(test_params)
+
     if @test.save
       redirect_to root_path
     else
       render :new # Just rendering view '/tests/new' using @test (action #new not used)
+    end
+  end
+
+  # Edit -> Update
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.update(test_params)
+      redirect_to root_path
+    else
+      render :edit
     end
   end
 
