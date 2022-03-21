1ee2709bf563a45a78005993b6a45306dd82102a
raven <vorontsof@mail.ru>
2022-03-22 03:57:21 +0400

    lesson8  Actions for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,44 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[show destroy]
-  before_action :find_test, only: %i[index create]
+  before_action :find_question, only: %i[show update destroy edit]
+  before_action :find_test, only: %i[index create new update destroy show edit]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    @questions= @test.questions
+    @questions = @test.questions
   end
 
   def show
     @question
   end
 
+  def new
+    @question = @test.questions.new
+  end
+
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to @question
+      redirect_to @test
     else
       render :new
     end
   end
 
+  # Edit -> Update
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @question.destroy
-    render plain: "Question was deleted!"
+    redirect_to @test
   end
 
   private
@@ -33,6 +48,7 @@ class QuestionsController < ApplicationController
   end
 
   def find_test
+    # @test = Test.find(@question[:test_id])
     @test = Test.find(params[:test_id])
   end
 
