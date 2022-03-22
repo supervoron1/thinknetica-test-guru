1ab243a5161e78d63ec3ad357b18ca6f1e377f8b
raven <vorontsof@mail.ru>
2022-03-22 13:57:46 +0400

    lesson8  Footer & app helpers added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(link_name, author, repo)
+    link_to link_name, "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
