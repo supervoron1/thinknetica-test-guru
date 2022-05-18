180fcf7e5b106193612ae557a2cc6cd1bd8895b4
raven <vorontsof@mail.ru>
2022-05-18 15:24:33 +0400

    lesson15  Sorting on tests page added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require('../utilities/sorting')
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,7 +13,6 @@ import "channels"
 // import "bootstrap/dist/js/bootstrap"
 // import "bootstrap/dist/css/bootstrap"
 import "bootstrap"
-import "stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
