8ec76afa0c6363197783555927dddd7432e0f5eb
raven <vorontsof@mail.ru>
2022-05-26 11:01:35 +0400

    lesson17 config autoloader

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module TestGuru
     config.i18n.default_locale = :ru
     config.autoload_paths << "#{Rails.root}/lib/clients"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoloader = :classic
   end
 end
