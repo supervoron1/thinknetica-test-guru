6222ebcc0776189392a37cfc9079f9ce916b743f
raven <vorontsof@mail.ru>
2022-06-14 12:04:44 +0400

    lesson18  All badges views added

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,10 @@
-class UserBadge < ApplicationRecord
-  belongs_to :user
-  belongs_to :badge
+class Badge < ApplicationRecord
+  has_many :user_badges, dependent: :destroy
+  has_many :users, through: :user_badges
+
+  validates :title, presence: true
+  validates :file_name, presence: true
+  validates :badge_type, presence: true
+
+  enum badge_type: { all_categories: 0, all_levels: 1, first_try: 2 }
 end
